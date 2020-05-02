7d3077262e60c39f9169f4c074b81213faa3d49b
jakub <jakubasek.m@gmail.com>
2020-05-02 18:49:39 +0200

    dokomentovavam leaderboard

--- src/tetris/leaderboard/LeaderboardData.java
+++ src/tetris/leaderboard/LeaderboardData.java
@@ -7,20 +7,43 @@ import javafx.beans.property.StringProperty;
 
 import java.io.Serializable;
 
+/**
+ * Představuje datový model pro položku v {@link javax.swing.text.TableView}
+ * reprezentující dosažené skóre hráče
+ *
+ * @author Martin Jakubašek
+ * @version 1.00.000
+ */
 public class LeaderboardData implements Serializable {
 
     public StringProperty playerName;
     public IntegerProperty score;
 
+    /**
+     * Vytvoří novou položku pro {@link javax.swing.text.TableView}
+     *
+     * @param playerName jméno hráče
+     * @param score dosažené skóre
+     */
     public LeaderboardData(String playerName, int score) {
         this.playerName = new SimpleStringProperty(playerName);
         this.score = new SimpleIntegerProperty(score);
     }
 
+    /**
+     * Vrátí {@link StringProperty} se jménem hráče
+     *
+     * @return {@link StringProperty} se jménem hráče
+     */
     public StringProperty playerNameProperty() {
         return playerName;
     }
 
+    /**
+     * Vrátí {@link IntegerProperty} se skórem hráče
+     *
+     * @return {@link IntegerProperty} se skórem hráče
+     */
     public IntegerProperty scoreProperty() {
         return score;
     }
--- src/tetris/leaderboard/OrderCell.java
+++ src/tetris/leaderboard/OrderCell.java
@@ -2,6 +2,12 @@ package tetris.leaderboard;
 
 import javafx.scene.control.TableCell;
 
+/**
+ * Reprezentuje buňku v {@link javafx.scene.control.TableView}, pro zobrazení pořadí prvků
+ *
+ * @author Martin Jakubašek
+ * @version 1.00.000
+ */
 public class OrderCell extends TableCell<LeaderboardData, Integer> {
 
     public void updateItem(Integer value, boolean empty) {
